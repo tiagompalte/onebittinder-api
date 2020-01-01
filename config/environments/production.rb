b7545d78d35a15a8cc98fc92960213483b968df5
Tiago Mariani Palte <tiagompalte@hotmail.com>
2020-01-01 16:10:16 -0300

    deploy heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,8 +34,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = 'wss://onebittinder-api.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [ 'https://onebittinder.herokuapp.com' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
